87d1ca1b1331d90d0c09eb8e29c893a7c3c6cd23
karvlox <135563580+Karvlox@users.noreply.github.com>
2025-05-31 01:01:56 -0400

    feat: Remove Atribute.

--- Models/Diagnostic.cs
+++ Models/Diagnostic.cs
@@ -1,9 +1,8 @@
 namespace BikeDoctor.Models;
 
 public class Diagnostic
-{    
+{
     public string Error { get; set; }
     public string DetailOfError { get; set; }
-    public string ServiceType  { get; set; }
     public int TimeSpent { get; set; } // minutes
 }
